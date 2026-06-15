caca21ddeebabec9bd0355d4c7443f191f45d49c
Matt Pruitt <matt@guitsaru.com>
2026-06-15 17:21:48 +0700

    test: pulsar.gen.button installs component test by default

---
+++ test/mix/tasks/pulsar.gen.component_test_install_test.exs
@@ -0,0 +1,22 @@
+defmodule Mix.Tasks.Pulsar.Gen.ComponentTestInstallTest do
+  use ExUnit.Case, async: true
+
+  import Igniter.Test
+
+  describe "pulsar.gen.button test installation" do
+    test "writes a component test file by default" do
+      phx_test_project()
+      |> Igniter.compose_task("pulsar.gen.button", [])
+      |> assert_creates("test/test_web/components/button_test.exs")
+      |> apply_igniter!()
+    end
+
+    test "--no-tests suppresses the component test file" do
+      igniter =
+        phx_test_project()
+        |> Igniter.compose_task("pulsar.gen.button", ["--no-tests"])
+
+      refute Map.has_key?(igniter.rewrite.sources, "test/test_web/components/button_test.exs")
+    end
+  end
+end
