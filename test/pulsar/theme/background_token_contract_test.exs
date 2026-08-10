810e992f9732de99f90b74ce0670334f3e3d2d43
Matt Pruitt <matt@guitsaru.com>
2026-08-10 22:38:21 +0700

    Clarify the page background token contract

---
+++ test/pulsar/theme/background_token_contract_test.exs
@@ -0,0 +1,23 @@
+defmodule Pulsar.Theme.BackgroundTokenContractTest do
+  use ExUnit.Case, async: true
+
+  @entry Path.expand("../../../priv/templates/theme.css.eex", __DIR__)
+  @light Path.expand("../../../priv/templates/themes/light.css.eex", __DIR__)
+  @dark Path.expand("../../../priv/templates/themes/dark.css.eex", __DIR__)
+
+  test "documents --color-background as the body page ground" do
+    css = File.read!(@entry)
+
+    assert css =~ "`--color-background` is the application/page ground"
+    assert css =~ "apply `bg-background` to `<body>`"
+  end
+
+  test "documents --color-surface-0 as the base elevation surface" do
+    for template <- [@light, @dark] do
+      css = File.read!(template)
+
+      assert css =~ ~r/--color-surface-0:[^;]+;\s*\/\* Base elevation surface \*\//
+      refute css =~ "Canvas/page background"
+    end
+  end
+end
